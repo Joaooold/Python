8f7b6d3025e6cdb72b6ef4d6a94ccde965d7ac2c
Joao Batista <joaofilhot09@gmail.com>
2024-11-13 18:46:01 -0300

    Initial commit

---
+++ projeto1/codigo.py
@@ -0,0 +1,6 @@
+# Passo 1: Entrar no sistema da empresa https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# pip install pyautogui
+# Passo 2: Fazer Login
+# Passo 3: Importar a base de dados
+# Passo 4: Repetir o processo de cadastro até acabar com os produtos
+import pyautogui
